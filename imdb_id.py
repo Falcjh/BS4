77947630142bb4068819df71bbe21c0b9e16871f
Falcjh <helsmoortel_jens@hotmail.com>
2019-06-05 06:31:06 +0200

    Second request working

--- imdb_id.py
+++ imdb_id.py
@@ -5,10 +5,9 @@ from random import randint
 
 
 # Collecting URL from the search page
-movie = "fc de kampioenen"
+movie = "the hulk"
 search_url = 'https://www.imdb.com/find?ref_=nv_sr_fn&q=' + movie + '&s=all'
 
-movie_id = []
 
 name = ''
 year = ''
@@ -23,10 +22,17 @@ search = soup.find('table')
 table = search.find_all('a', limit=1)
 # extract the movie id from the href and store it in a list to use it later.
 for link in table:
-    movie_id.append(link.get('href'))
+    movie_id = (link.get('href'))
     print(movie_id)
 # put the script to sleep so it looks more human
     for z in range(0, 3):
         print("acting human")
         sleep(randint(1, 4))
 # make another request to the movie info page
+    movie_url = 'https://www.imdb.com' + movie_id + '?ref_=fn_al_tt_1'
+    page2 = requests.get(movie_url)
+
+    soup2 = BeautifulSoup(page2.text, 'html.parser')
+    search_name = soup2.find('div', class_='title_wrapper')
+
+    print(search_name)
